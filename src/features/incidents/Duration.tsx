a43b40bc21c396fc48aa477f0df6216f84b56b0f
Vinay Raghu <hello@vinayraghu.com>
2020-08-23 12:16:25 -0400

    add duration component

--- src/features/incidents/Duration.tsx
+++ src/features/incidents/Duration.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { getDaysHoursMinutesSeconds } from "./incidents.helpers";
 
 interface DurationInterface {
   duration: number;
 }
 
 const Duration = ({ duration }: DurationInterface) => {
-  return <p>{duration}</p>;
+  const { days, hours, minutes, seconds } = getDaysHoursMinutesSeconds(
+    duration
+  );
+
+  const daysDisplay = days > 0 ? `${days} day${days > 1 && "s"} ` : null;
+  const hoursDisplay = hours > 0 ? `${hours} hour${hours > 1 && "s"} ` : null;
+  const minutesDisplay =
+    minutes > 0 ? `${minutes} minute${minutes > 1 && "s"} ` : null;
+  const secondsDisplay =
+    seconds > 0 ? `${seconds} second${seconds > 1 && "s"} ` : null;
+
+  return (
+    <div>
+      {daysDisplay}
+      {hoursDisplay}
+      {minutesDisplay}
+      {secondsDisplay}
+    </div>
+  );
 };
 
 export default Duration;
